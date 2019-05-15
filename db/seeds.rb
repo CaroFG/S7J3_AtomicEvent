1b4ee7c7c630d91ce8f054ee7c50071db5b33ca1
CaroFG <carolina.ferreira131@gmail.com>
2019-05-15 17:22:22 +0200

    avatar ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,36 +3,35 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#  movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#  Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 User.destroy_all
 Event.destroy_all
-
 #----------------On crée nos users et nos events -----------------
 10.times do
-	@user = User.create!(
-		first_name: Faker::Name.first_name,
-		last_name: Faker::Name.last_name, 
-		email: Faker::Internet.email,
-		description: Faker::Movie.quote, 
-		password: Faker::Internet.password
-	)	
-	puts 'user added'
-	@user.avatar.attach(io: File.open('app/assets/images/event.jpg'), filename: 'event.jpg')
-
-	@event = Event.new(
-		start_date: "2019-06-21 19:00",
-		duration: 50,
-		title: Faker::Movie.quote,
-		description: Faker::Lorem.paragraph_by_chars(55, false),
-		price: rand(1..1000),
-		location: Faker::Address.city,
-		admin: @user
-	)
-	
-	@event.picture.attach(io: File.open('app/assets/images/event.jpg'), filename: 'event.jpg')
-	
-	@event.save
-
+  @user = User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name, 
+    email: Faker::Internet.email,
+    description: Faker::Movie.quote, 
+    password: Faker::Internet.password
+    )  
+  puts 'user added'
+  @user.avatar.attach(io: File.open('app/assets/images/event.jpg'), filename: 'event.jpg')
+  @event = Event.new(
+    start_date: "2019-06-21 19:00",
+    duration: 50,
+    title: Faker::Movie.quote,
+    description: Faker::Lorem.paragraph_by_chars(55, false),
+    price: rand(1..1000),
+    location: Faker::Address.city,
+    admin: @user
+    )
+  
+  # A supprimer, gardé pour tests
+  @event.update(validated:true)
+  @event.picture.attach(io: File.open('app/assets/images/event.jpg'), filename: 'event.jpg')
+  @event.save
+  puts 'Event added'
 end
